2f85c59da265ec076d91749af0537d605f81290a
cristianvilela0K@gmail.com <cristianvilela0K@gmail.com>
2020-07-27 16:10:57 -0300

    + TypeScript

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+	dadosRepublicas = [];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
-
+  	ngOnInit() {
+  		this.dadosRepublicas = [
+  			{
+  				nomeRepublica: 'RepublicaDosZé',
+				endereco: 'Rua dos Zes',
+				quantidadeDeQuartos: '5',
+				preco: '600',
+			},
+			{
+				nomeRepublica: 'RepublicaDoisZé',
+				endereco: 'Rua dos Zes',
+				quantidadeDeQuartos: '1',
+				preco: '800',
+			},
+			{
+  				nomeRepublica: 'RepublicaDosZézin',
+				endereco: 'Rua dos Zes',
+				quantidadeDeQuartos: '5',
+				preco: '700',
+			},
+			{
+  				nomeRepublica: 'RepublicaDosZézão',
+				endereco: 'Rua dos Zes',
+				quantidadeDeQuartos: '3',
+				preco: '500',
+			},
+			{
+  				nomeRepublica: 'RepublicaDosZéca',
+				endereco: 'Rua dos Zes',
+				quantidadeDeQuartos: '2',
+				preco: '400',
+			},
+  		]
+	}
 }
